5ae3d7a46a40853abf477d6d29d14fd1ed42e904
jasonpanosso <jasonpanosso@protonmail.com>
2024-02-26 17:27:50 -0500

    nvim: quickfix indent-blankline

--- programs/nvim/plugins/indent-blankline.nix
+++ programs/nvim/plugins/indent-blankline.nix
@@ -1,7 +1,7 @@
 { ... }:
 
 {
-  programs.nixvim.indent-blankline = {
+  programs.nixvim.plugins.indent-blankline = {
     enable = true;
     exclude = {
       buftypes = [ "terminal" "nofile" "prompt" ];
