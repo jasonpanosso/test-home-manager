43a34843c9f9cde56c867fdc1b59a3dd1d402292
jasonpanosso <jasonpanosso@protonmail.com>
2024-02-27 13:17:49 -0500

    nvim: properly declare pkg dependencies

--- programs/nvim/plugins/lint.nix
+++ programs/nvim/plugins/lint.nix
@@ -1,26 +1,28 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    nodePackages_latest.eslint_d
-    selene
-    python311Packages.flake8
-    sqlfluff
-    ansible-lint
-    djlint
-  ];
+  programs.nixvim = {
+    extraPackages = with pkgs;[
+      nodePackages_latest.eslint_d
+      selene
+      python311Packages.flake8
+      sqlfluff
+      ansible-lint
+      djlint
+    ];
 
-  programs.nixvim.plugins.lint = {
-    enable = true;
-    autoCmd.event = [ "InsertLeave" "TextChanged" "BufRead" ];
-    lintersByFt = {
-      python = [ "flake8" ];
-      lua = [ "selene" ];
-      luau = [ "selene" ];
-      sql = [ "sqlfluff" ];
-      yml = [ "ansible_lint" ];
-      yaml = [ "ansible_lint" ];
-      htmldjango = [ "djlint" ];
+    plugins.lint = {
+      enable = true;
+      autoCmd.event = [ "InsertLeave" "TextChanged" "BufRead" ];
+      lintersByFt = {
+        python = [ "flake8" ];
+        lua = [ "selene" ];
+        luau = [ "selene" ];
+        sql = [ "sqlfluff" ];
+        yml = [ "ansible_lint" ];
+        yaml = [ "ansible_lint" ];
+        htmldjango = [ "djlint" ];
+      };
     };
   };
 }
