71781ddfe0963855a3ee5896cf50cffc752b54c7
jasonpanosso <jasonpanosso@protonmail.com>
2024-02-27 11:22:20 -0500

    nvim: setup lspsaga

--- programs/nvim/lsp/default.nix
+++ programs/nvim/lsp/default.nix
@@ -7,5 +7,28 @@
 
   imports = [
     ./inlayhints
+    ./lspsaga
+  ];
+
+  programs.nixvim.keymaps = [
+    {
+      mode = [ "n" ];
+      key = "<leader>li";
+      action = "<cmd>LspInfo<CR>";
+      options = {
+        silent = true;
+        desc = "[L]SP [I]nfo";
+      };
+    }
+
+    {
+      mode = [ "n" ];
+      key = "<leader>k";
+      action = "<cmd>lua vim.lsp.buf.signature_help()<CR>";
+      options = {
+        silent = true;
+        desc = "LSP: Signature Help";
+      };
+    }
   ];
 }
---
+++ programs/nvim/lsp/lspsaga/default.nix
@@ -0,0 +1,35 @@
+{ ... }:
+
+{
+  imports = [ ./keybinds.nix ];
+  programs.nixvim.plugins.lspsaga = {
+    enable = true;
+
+    ui = {
+      title = false;
+      border = "rounded";
+    };
+
+    rename = {
+      inSelect = false;
+      autoSave = true;
+      keys = {
+        quit = "<esc>";
+      };
+    };
+
+    hover = {
+      openLink = "<CR>";
+      openCmd = "!firefox";
+    };
+
+    symbolInWinbar = {
+      separator = " ";
+      hideKeyword = true;
+      folderLevel = 2;
+    };
+
+    beacon.enable = false;
+    lightbulb.enable = false;
+  };
+}
