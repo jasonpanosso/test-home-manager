55589e64cb4a63801cf5cef97c12b4e7a571087e
jasonpanosso <jasonpanosso@protonmail.com>
2024-02-27 18:04:59 -0500

    nvim: improve gitsign rendering

--- programs/nvim/plugins/gitsigns.nix
+++ programs/nvim/plugins/gitsigns.nix
@@ -3,7 +3,38 @@
     enable = true;
     currentLineBlame = true;
     signcolumn = true;
+
     signs = {
+      add = {
+        hl = "GitSignsAdd";
+        text = "▎";
+        numhl = "GitSignsAddNr";
+        linehl =
+          "GitSignsAddLn";
+      };
+
+      change = {
+        hl = "GitSignsChange";
+        text = "▎";
+        numhl = "GitSignsChangeNr";
+        linehl = "GitSignsChangeLn";
+      };
+
+      delete = {
+        hl = "GitSignsDelete";
+        text = "契";
+        numhl = "GitSignsDeleteNr";
+        linehl = "GitSignsDeleteLn";
+      };
+
+      topdelete = {
+        hl = "GitSignsDelete";
+        text = "契";
+        numhl =
+          "GitSignsDeleteNr";
+        linehl = "GitSignsDeleteLn";
+      };
+
       changedelete = {
         hl = "GitSignsChange";
         text = "▎";
