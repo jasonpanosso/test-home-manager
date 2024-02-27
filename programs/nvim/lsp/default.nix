01648947f20c4864e220adebb277eb67a9154c25
jasonpanosso <jasonpanosso@protonmail.com>
2024-02-27 18:19:09 -0500

    nvim: cleanup lsp config, prettify diagnostics

--- programs/nvim/lsp/default.nix
+++ programs/nvim/lsp/default.nix
@@ -1,10 +1,46 @@
 {
-  programs.nixvim.plugins.lsp = {
-    enable = true;
-    capabilities = #lua
-      ''
-        require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-      '';
+  programs.nixvim = {
+    plugins.lsp = {
+      enable = true;
+      capabilities = #lua
+        ''
+          require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+        '';
+    };
+
+    extraConfigLua = builtins.readFile ./prettify_lsp.lua;
+
+    keymaps = [
+      {
+        mode = [ "n" ];
+        key = "<leader>li";
+        action = "<cmd>LspInfo<CR>";
+        options = {
+          silent = true;
+          desc = "[L]SP [I]nfo";
+        };
+      }
+
+      {
+        mode = [ "n" "v" ];
+        key = "<leader>lr";
+        action = "<cmd>LspStop<CR><cmd>sleep 100m<CR><cmd>LspStart<CR>";
+        options = {
+          desc = "[L]sp [R]estart";
+        };
+      }
+
+      {
+        mode = [ "n" ];
+        key = "<leader>k";
+        action = "vim.lsp.buf.signature_help";
+        lua = true;
+        options = {
+          silent = true;
+          desc = "LSP: Signature Help";
+        };
+      }
+    ];
   };
 
   imports = [
@@ -15,37 +51,5 @@
     ./rustaceanvim.nix
     ./typescript-tools.nix
     ./roslyn.nix
-  ];
-
-  programs.nixvim.keymaps = [
-    {
-      mode = [ "n" ];
-      key = "<leader>li";
-      action = "<cmd>LspInfo<CR>";
-      options = {
-        silent = true;
-        desc = "[L]SP [I]nfo";
-      };
-    }
-
-    {
-      mode = [ "n" "v" ];
-      key = "<leader>lr";
-      action = "<cmd>LspStop<CR><cmd>sleep 100m<CR><cmd>LspStart<CR>";
-      options = {
-        desc = "[L]sp [R]estart";
-      };
-    }
-
-    {
-      mode = [ "n" ];
-      key = "<leader>k";
-      action = "vim.lsp.buf.signature_help";
-      lua = true;
-      options = {
-        silent = true;
-        desc = "LSP: Signature Help";
-      };
-    }
   ];
 }
