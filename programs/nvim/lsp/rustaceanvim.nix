2f90ea89240056889a5ef95a2a80eb9c91a48ba7
jasonpanosso <jasonpanosso@protonmail.com>
2024-03-14 13:28:38 -0400

    nvim: quickfix highlights

--- programs/nvim/lsp/rustaceanvim.nix
+++ programs/nvim/lsp/rustaceanvim.nix
@@ -16,6 +16,10 @@
             vim.keymap.set('n', '<Leader>ca', function()
               vim.cmd.RustLsp('codeAction')
             end, { buffer = bufnr, silent = true })
+
+            # https://www.reddit.com/r/neovim/comments/12gvms4/this_is_why_your_higlights_look_different_in_90/
+            vim.api.nvim_set_hl(0, '@lsp.type.macro', { link = 'Macro', default = true })
+            vim.api.nvim_set_hl(0, '@lsp.type.decorator', { link = '@function', default = true })
           end
         '';
 
